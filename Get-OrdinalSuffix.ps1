d5063b9132a7de6a7880d0dd55599164093c4ca0
Tony Pagliaro (RFA) <tpagliaro@rfa.com>
2018-09-23 14:58:54 -0400

    new functions for number ordinals

---
+++ Get-OrdinalSuffix.ps1
@@ -0,0 +1,15 @@
+# Based off of https://gist.github.com/markwragg/8c24a6462feee7849ba7e06b0b0782fa Get-OrdinalNumber
+Function Get-OrdinalSuffix {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [int64]$num
+    )
+
+    $Suffix = Switch -regex ($Num) {
+        '.?1$'      { 'st'; break }
+        '.?2$'      { 'nd'; break }
+        '.?3$'      { 'rd'; break }
+        default     { 'th'; break }
+    }
+    Write-Output "$Suffix"
+}
