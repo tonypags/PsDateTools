e6d967d0291ee56fb37c0e7d1a0c0a00c214e633
Tony Pagliaro (RFA) <tpagliaro@rfa.com>
2018-09-23 15:16:54 -0400

    added pipeline support

--- Add-OrdinalSuffix.ps1
+++ Add-OrdinalSuffix.ps1
@@ -1,15 +1,20 @@
 # Based off of https://gist.github.com/markwragg/8c24a6462feee7849ba7e06b0b0782fa Get-OrdinalNumber
 Function Add-OrdinalSuffix {
     Param(
-        [Parameter(Mandatory=$true)]
-        [int64]$num
+        [Parameter(Mandatory=$true,
+                    ValueFromPipeline=$true)]
+        [int64[]]$Num
     )
 
-    $Suffix = Switch -regex ($Num) {
-        '.?1$'      { 'st'; break }
-        '.?2$'      { 'nd'; break }
-        '.?3$'      { 'rd'; break }
-        default     { 'th'; break }
+    Process {
+        foreach ($N in $Num) {
+            $Suffix = Switch -regex ($N) {
+                '.?1$'      { 'st'; break }
+                '.?2$'      { 'nd'; break }
+                '.?3$'      { 'rd'; break }
+                default     { 'th'; break }
+            }
+            Write-Output "$N$Suffix"
+        }
     }
-    Write-Output "$Num$Suffix"
 }
