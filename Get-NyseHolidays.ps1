ffe3c1c68b971296ea4365d87684b171849936cb
Tony Pagliaro (RFA) <tpagliaro@rfa.com>
2018-11-18 20:57:15 -0500

    Issue: not working due to new column headers fix: Add logic to handle case where column headers exist.

--- Get-NyseHolidays.ps1
+++ Get-NyseHolidays.ps1
@@ -5,20 +5,52 @@ function Get-NyseHolidays {
         
     )
     
-    process {
+    Process {
         $Url = 'https://www.nyse.com/markets/hours-calendars'
         $SiteReply = Invoke-WebRequest -Uri $Url -DisableKeepAlive
         $rawTable = $SiteReply | Get-WebRequestTable
+        $rawHeaders = $rawTable |
+            Get-Member |
+            Where-Object {$_.MemberType -eq 'NoteProperty'} |
+            Select -ExpandProperty Name
 
-        # 1st record is column headers
-        $ColumnOrder = $rawTable[0] | foreach {$_.P1,$_.P2,$_.P3,$_.P4}
+        if ($rawHeaders -contains 'P4') {
 
-        # All Dates can now be resolved from remaining rows
-        $numRecords = $rawTable.count
-        $rawTable[1..$numRecords] | Foreach-Object {
-            Get-Date "$($_.P2.TrimEnd('*') -replace '\s\(.*\)') $($ColumnOrder[1])"
-            Get-Date "$($_.P3.TrimEnd('*') -replace '\s\(.*\)') $($ColumnOrder[2])"
-            Get-Date "$($_.P4.TrimEnd('*') -replace '\s\(.*\)') $($ColumnOrder[3])"
-        }
+            # This is the way to handle the table where there are no column headers
+            # 1st record is column headers
+            $ColumnOrder = $rawTable[0] | foreach {$_.P1,$_.P2,$_.P3,$_.P4}
+
+            # All Dates can now be resolved from remaining rows
+            $numRecords = $rawTable.count
+            $rawTable[1..$numRecords] | Foreach-Object {
+                Get-Date "$($_.P2.TrimEnd('*') -replace '\s\(.*\)') $($ColumnOrder[1])"
+                Get-Date "$($_.P3.TrimEnd('*') -replace '\s\(.*\)') $($ColumnOrder[2])"
+                Get-Date "$($_.P4.TrimEnd('*') -replace '\s\(.*\)') $($ColumnOrder[3])"
+            }
+        } elseif ($rawHeaders -contains 'HOLIDAY') {
+            # Make an emply collection
+            $DateSet = New-Object System.Collections.ArrayList
+            
+            # Get the year numbers from the column headers
+            $YearSet  = ($rawHeaders | Sort-Object)[0..2]
+
+            # Get the Holiday names
+            $HolidayNameSet = $rawTable.HOLIDAY
+            
+            # Loop thru each year and return all dates to the collection
+            Foreach ($Year in $YearSet) {
+                Foreach ($Holiday in $HolidayNameSet) {
+                    $rawYearlessDate = $rawTable |
+                        Where-Object {$_.HOLIDAY -eq $Holiday } |
+                        Select-Object -ExpandProperty $Year
+                    $YearlessDate = $rawYearlessDate.TrimEnd('*') -replace '\s\(.*\)'
+                    $DateToAdd = Get-Date "$($YearlessDate) $($Year)"
+                    [void]($DateSet.Add($DateToAdd))
+                }
+            }#Foreach ($Year in $YearSet) {
+            Write-Output $DateSet
+        } else {
+            throw 'No Match for table handler!'
+        }#if ($rawHeaders -contains 'P4') {
     }
 }
