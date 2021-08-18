b0dc7eec028fec7dc598fcd636887db2e0df2a98
Tony Pagliaro <Tony@Pagliaro.co>
2021-08-18 17:48:55 -0400

    Remove call to private folder that doesn't exist.

--- PsDateTools.psm1
+++ PsDateTools.psm1
@@ -8,7 +8,7 @@
 
 #Get public and private function definition files.
     $Public  = Get-ChildItem $PSScriptRoot\*.ps1 -ErrorAction SilentlyContinue
-    $Private  = Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue
+    # $Private  = Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue
 
 #Dot source the files
     Foreach($import in @($Public))
