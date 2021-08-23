d9b79140a4382574ac3a5aa6a12d326d4e492577
Tony Pagliaro <Tony@Pagliaro.co>
2021-08-23 15:54:09 -0400

    depreciation notes

--- Convert-UtcToLocal.ps1
+++ Convert-UtcToLocal.ps1
@@ -1,17 +1,7 @@
 function Convert-UtcToLocal {
     <#
     .SYNOPSIS
-    Converts a datetime object in UTC to your local timezone
-    .DESCRIPTION
-    Converts a datetime object in UTC to your local timezone
-    .PARAMETER UtcDate
-    A [datetime] object that represents a time in Universal Coordinated Time.
-    .EXAMPLE
-    Convert-UtcToLocal -UtcDate 'Tuesday, August 3, 2021 3:59:55 PM'
-    .EXAMPLE
-    $localDate = [datetime]$UtcDate | Convert-UtcToLocal
-    .NOTES
-    Taken from https://devblogs.microsoft.com/scripting/powertip-convert-from-utc-to-my-local-time-zone/
+    DEPRECIATED: Use 'ConvertTo-LocalTime -Utc' instead
     #>
     [CmdletBinding()]
     param(
