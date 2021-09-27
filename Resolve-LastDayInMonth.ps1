8da4b1f5808d783e0435fd6c2037f7e9f5d6c74b
Tony Pagliaro <tony@pagliaro.co>
2021-09-27 14:39:42 -0400

    Replace janky logic with .Net method.

--- Resolve-LastDayInMonth.ps1
+++ Resolve-LastDayInMonth.ps1
@@ -2,5 +2,5 @@ function Resolve-LastDayInMonth {
     param (
         [datetime]$Date = (Get-Date)
     )
-    ($Date.AddMonths(1) | Get-TruncatedDate -Truncate Day).AddDays(-1).Day
+    [datetime]::DaysInMonth(($Date.Year),($Date.Month))
 }
