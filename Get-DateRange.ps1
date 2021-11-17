78a91cdceea71ed485cd5cdcfbfcb10d467e0588
Tony Pagliaro <tony@pagliaro.co>
2021-11-17 11:23:18 -0500

    Add handling for out-of-order dates. Change array to arraylist

--- Get-DateRange.ps1
+++ Get-DateRange.ps1
@@ -1,30 +1,26 @@
-<#
-.Synopsis
-   Returns a series of consecutive dates when given a start and end date. 
-.EXAMPLE
-   Get-DateRange -StartDate '1/1/2017' -EndDate '1/4/2017'
-   1/1/2017
-   1/2/2017
-   1/3/2017
-   1/4/2017
-.INPUTS
-   Inputs to this cmdlet are not accepted. 
-.OUTPUTS
-   Output from this cmdlet is an array of datetime objects. 
-#>
-function Get-DateRange
-{
+function Get-DateRange {
+    <#
+    .Synopsis
+    Returns a series of consecutive dates when given a start and end date. 
+    .EXAMPLE
+    Get-DateRange -StartDate '1/1/2017' -EndDate '1/4/2017'
+    1/1/2017
+    1/2/2017
+    1/3/2017
+    1/4/2017
+    .INPUTS
+    Inputs to this cmdlet are not accepted. 
+    .OUTPUTS
+    Output from this cmdlet is an array of datetime objects. 
+    #>
     [CmdletBinding()]
-    [OutputType([datetime[]])] 
     Param
     (
-        [Parameter(Mandatory=$true, 
-                   Position=0)]
+        [Parameter(Mandatory,Position=0)]
         [datetime]
         $StartDate,
         
-        [Parameter(Mandatory=$true, 
-                   Position=1)]
+        [Parameter(Mandatory,Position=1)]
         [datetime]
         $EndDate,
         
@@ -33,34 +29,27 @@ function Get-DateRange
         $Extremes
     )
 
+    $DateArray = [system.collections.arraylist]@()
+    for 
+    (
+        $i = $StartDate;
+        $i -le $EndDate;
+        $i = $i.AddDays(1)
+    )
+    { [void]($DateArray.Add(($i | Get-TruncatedDate -Truncate Hour )))}
 
-
-    Begin
-    {
+    if($Extremes){
+        $FullArray = $DateArray | Sort-Object
         $DateArray = @()
+        $DateArray += $FullArray[0]
+        $DateArray += $FullArray[-1]
+        $DateArray =  $DateArray | Sort-Object
     }
-    Process
-    {
-    }
-    End
-    {
-        for 
-        (
-            $i = $StartDate
-            $i -le $EndDate
-            $i = $i.AddDays(1)
-        )
-        { $DateArray += [datetime]$i.ToShortDateString() }
 
-        if($Extremes){
-            $FullArray = $DateArray | sort
-            $DateArray = @()
-            $DateArray += $FullArray[0]
-            $DateArray += $FullArray[-1]
-            $DateArray =  $DateArray | sort
-        }
-
-        Write-Output $DateArray
+    if ($StartDate -gt $EndDate) {
+        Write-Warning "No output is expected with StartDate > EndDate!"
+    } else {
+        $DateArray
     }
+    
 }
-
