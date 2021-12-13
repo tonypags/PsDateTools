92648e4aa82253a4a715fc2d72ae06e7f47c2c07
Tony Pagliaro <tony@pagliaro.co>
2021-12-13 17:02:06 -0500

    New function

---
+++ Find-TimeInDay.ps1
@@ -0,0 +1,141 @@
+function Find-TimeInPastDay {
+    <#
+    .SYNOPSIS
+    Finds the datetime value of the most recent given time
+
+    .DESCRIPTION
+    Given a time (string HH:mm[:ss[.fff]]) and reference datetime,
+    it finds a new datetime in the past equal to the given time.
+
+    .PARAMETER Time
+    The time (HH:mm[:ss[.fff]] string)
+
+    .PARAMETER Date
+    Datetime used as the day's reference point
+
+    .PARAMETER Skip
+    Number of days to skip
+
+    .EXAMPLE
+    ($Date = Get-Date);$time = '23:55'
+    #Monday, December 13, 2021 4:15:31 PM
+    Find-TimeInPastDay $time -Date $Date -Debug
+    #>
+    [CmdletBinding()]
+    param (
+        # The time (HH:mm[:ss[.fff]] string)
+        [Parameter(Mandatory)]
+        [string]
+        $Time,
+
+        # Datetime used as the day's reference point
+        [Parameter(Mandatory)]
+        [datetime]
+        $Date,
+
+        # Number of days to skip
+        [Parameter()]
+        [ValidateNotNull()]
+        [int]
+        $Skip = 0
+    )
+
+    # Handle the Skip parameter first
+    $Date = $Date.AddDays(-$Skip)
+
+    # Seconds & Milliseconds are optional on the parameter string value
+    # Find the time-parts like parameter values for Get-Date
+    $timeParts = $Time -split ':'
+    $timeToFind = @{}
+    $timeToFind.Hour = $timeParts[0] -as [int]
+    $timeToFind.Minute = $timeParts[1] -as [int]
+    if ($timeParts[2]) {$timeToFind.Second = $timeParts[2] -as [int]}
+    if ($timeParts[3]) {$timeToFind.Millisecond = $timeParts[3] -as [int]}
+
+    # Go to previous day if the reference time is lower than the target time
+    if (
+        $Date.Hour -lt $timeToFind.Hour -or
+        (
+            $Date.Hour -eq $timeToFind.Hour -and
+            $Date.Minute -lt $timeToFind.Minute
+        )
+    ) {
+        $Date = $Date | Get-TruncatedDate -Truncate 'Hour'
+        $Date = $Date.AddMilliseconds(-1)
+    }
+
+    While (
+        # Subtract 1 Hour until we get within 1 of the desired time
+        $Date.Hour -gt $timeToFind.Hour + 1
+    ) {
+        $Date = $Date.AddHours(-1)
+    }
+    Write-Debug "Hour +1 Found: $($Date.Hour)"
+
+
+    Do {
+        $Date = $Date.AddMinutes(-1)
+    } While (
+        # Subtract 1 Minute until we get within 1 of the desired time
+        $Date.Minute -gt $timeToFind.Minute + 1 -or
+        $Date.Hour -ne $timeToFind.Hour
+    )
+    Write-Debug "Minute +1 Found: $($Date.Minute)"
+
+
+    if ($timeToFind.Second) {
+
+        While (
+            # Subtract 1 Second until we get within 1 of the desired time
+            $Date.Second -gt $timeToFind.Second + 1 -or
+            $Date.Minute -ne $timeToFind.Minute
+        ) {
+            $Date = $Date.AddSeconds(-1)
+        }
+        Write-Debug "Second +1 Found: $($Date.Second)"
+
+    } else {
+
+        While (
+            # Subtract 1 Minute until we get within 1 of the desired time
+            $Date.Minute -ne $timeToFind.Minute -or
+            $Date.Hour -ne $timeToFind.Hour
+        ) {
+            $Date = $Date.AddMinutes(-1)
+        }
+        Write-Debug "Minute Finalized: $($Date.Minute)"
+
+    }
+
+
+    if ($timeToFind.Millisecond) {
+
+        While (
+            # Subtract 1 Millisecond until we get within 1 of the desired time
+            $Date.Millisecond -ne $timeToFind.Millisecond -or
+            $Date.Second -ne $timeToFind.Second
+        ) {
+            $Date = $Date.AddMilliseconds(-1)
+        }
+        Write-Debug "Millisecond Finalized: $($Date.Millisecond)"
+
+    } else {
+
+        if ($timeToFind.Second) {
+
+            While (
+                # Subtract 1 Second until we get within 1 of the desired time
+                $Date.Second -ne $timeToFind.Second -or
+                $Date.Minute -ne $timeToFind.Minute
+            ) {
+                $Date = $Date.AddSeconds(-1)
+            }
+            Write-Debug "Second Finalized: $($Date.Second)"
+        
+        }
+        
+    }
+
+    $Date
+
+}#END: function Find-TimeInPastDay
