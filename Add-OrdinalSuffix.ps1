0136570237d09aa3e92b0a676629be3faefb2fcd
Tony Pagliaro <tony@pagliaro.co>
2023-12-12 16:50:50 -0500

    Fix function call

--- Add-OrdinalSuffix.ps1
+++ Add-OrdinalSuffix.ps1
@@ -1,15 +1,15 @@
 # Based off of https://gist.github.com/markwragg/8c24a6462feee7849ba7e06b0b0782fa Get-OrdinalNumber
-Function Add-OrdinalSuffix {
+function Add-OrdinalSuffix {
+    [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$true,
-                    ValueFromPipeline=$true)]
-        [int64[]]$Num
+        [Parameter(ValueFromPipeline)]
+        [int64[]]
+        $Num
     )
 
     Process {
         foreach ($N in $Num) {
-            $Suffix = Get-OrdinalSuffix
-            Write-Output "$N$Suffix"
+            "$($N)$(Get-OrdinalSuffix $N)"
         }
     }
 }
